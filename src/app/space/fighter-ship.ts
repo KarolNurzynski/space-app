b8e210d43ab0e7820c28d286940b3a70f01fc586
Karol Nurzynski <karolnurzynski@gmail.com>
2022-12-14 14:29:21 +0100

    add ship classes

---
+++ src/app/space/fighter-ship.ts
@@ -0,0 +1,7 @@
+import {SpaceShip} from './space-ship';
+
+export class FighterShip extends SpaceShip {
+  constructor() {
+    super('Cyber', '/assets/cyber-space-ship.gif');
+  }
+}
---
+++ src/app/space/space-ship.ts
@@ -0,0 +1,13 @@
+export class SpaceShip {
+
+  constructor(modelName: string, imageUrl: string) {
+    this.modelName = modelName;
+    this.imageUrl = imageUrl;
+  }
+
+  modelName: string;
+  imageUrl: string;
+  health = 100;
+  activeShields = true;
+  activeWeapons = true;
+}
