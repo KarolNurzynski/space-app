f0a5eb82b78d5a3594008005c4a53302a351083e
Karol Nurzynski <karolnurzynski@gmail.com>
2022-12-14 10:50:53 +0100

    add a2s component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {A2sCommModule} from 'a2s-comm';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    A2sCommModule
   ],
   providers: [],
   bootstrap: [AppComponent]
