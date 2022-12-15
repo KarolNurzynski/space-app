944fc12b0cf4d0be4380a42dd72ff3abb00a5804
Karol Nurzynski <karolnurzynski@gmail.com>
2022-12-15 09:35:54 +0100

    assign pilot from hangar

--- src/app/space/hangar/hangar.component.ts
+++ src/app/space/hangar/hangar.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {SpaceShip} from '../space-ship';
 import {FighterShip} from '../fighter-ship';
 import {BomberShip} from '../bomber-ship';
 import {Pilot} from '../pilot';
+import {PilotRoomComponent} from '../pilot-room/pilot-room.component';
 
 @Component({
   selector: 'app-hangar',
@@ -11,9 +12,27 @@ import {Pilot} from '../pilot';
 })
 export class HangarComponent implements OnInit {
   spaceShips: SpaceShip[] = [];
+  selectedPilot: Pilot | null = null;
+  @ViewChild(PilotRoomComponent) pilotRoom!: PilotRoomComponent;
 
   ngOnInit(): void {
     this.spaceShips.push(new FighterShip());
     this.spaceShips.push(new BomberShip(new Pilot('Super Pilot', '/assets/pilot.jpeg')));
+  }
+
+  setSelectedPilot(pilot: Pilot | null) {
+    this.selectedPilot = pilot;
+  }
+
+  deassignPilot(spaceShip: SpaceShip) {
+    if (!spaceShip.pilot) { return; }
+    this.pilotRoom.pilotReturn(spaceShip.pilot);
+    spaceShip.pilot = undefined;
+  }
+
+  assignPilot(spaceShip: SpaceShip): void {
+    if (!this.selectedPilot) { return; }
+    spaceShip.pilot = this.selectedPilot;
+    this.pilotRoom.pilotLeave(this.selectedPilot);
   }
 }
--- src/app/space/pilot-room/pilot-room.component.ts
+++ src/app/space/pilot-room/pilot-room.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Pilot} from '../pilot';
 
 @Component({
@@ -9,6 +9,7 @@ import {Pilot} from '../pilot';
 export class PilotRoomComponent {
   pilots: Pilot[] = [];
   selectedPilot: Pilot | null = null;
+  @Output() selected = new EventEmitter<Pilot | null>();
 
   ngOnInit() {
     this.pilots.push(new Pilot('Karol', '/assets/pilot.jpeg'));
@@ -17,5 +18,16 @@ export class PilotRoomComponent {
 
   select(pilot: Pilot | null): void {
     this.selectedPilot = pilot;
+    this.selected.emit(pilot);
+  }
+
+  pilotReturn(pilot: Pilot) {
+    this.pilots.push(pilot);
+  }
+
+  pilotLeave(pilot: Pilot): void {
+    const index = this.pilots.indexOf(pilot);
+    this.pilots.splice(index, 1);
+    this.select(null);
   }
 }
